40f7a19df237c4e25e88c1a253c5ede2441f6e18
rafaa1999 <raferhouma10@gmail.com>
2022-09-10 17:37:47 +0100

    day one

---
+++ day_one/triangle.c
@@ -0,0 +1,34 @@
+/**
+ *
+ * write a programme that takes C column and shows this triangle:
+ *
+ *      *
+ *      **
+ *      ***
+ *      ****
+ *      ***
+ *      **
+ *      *
+ *
+ * **/
+
+
+ #include <stdio.h>
+int main() {
+   int i,j,C,P;
+   P=1;
+   printf("give me the number of col:");
+   scanf("%d",&C);
+   for(i=1;i<=C;i++){
+    for(j=1;j<=P;j++){
+        printf("* ");
+    }
+    if(i<C){
+        P++;
+    }else
+        P--;
+
+    printf("\n");
+   }
+   return 0;
+}
